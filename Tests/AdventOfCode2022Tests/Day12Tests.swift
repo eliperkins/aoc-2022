f49f2140b3b210ac003acb90df7d2144bb902a31
Eli Perkins <eli.j.perkins@gmail.com>
2022-12-13 12:52:13 -0500

    format

--- Tests/AdventOfCode2022Tests/Day12Tests.swift
+++ Tests/AdventOfCode2022Tests/Day12Tests.swift
@@ -1,22 +1,22 @@
 #if canImport(GameKit)
-import AdventOfCode2022
-import XCTest
+    import AdventOfCode2022
+    import XCTest
 
-final class Day12Tests: XCTestCase {
-    func test_part1_test() throws {
-        XCTAssertEqual(try Day12(input: Day12.sample).solvePart1(), 31)
-    }
+    final class Day12Tests: XCTestCase {
+        func test_part1_test() throws {
+            XCTAssertEqual(try Day12(input: Day12.sample).solvePart1(), 31)
+        }
 
-    func test_part1_solution() throws {
-        XCTAssertEqual(try Day12().solvePart1(), 339)
-    }
+        func test_part1_solution() throws {
+            XCTAssertEqual(try Day12().solvePart1(), 339)
+        }
 
-    func test_part2_test() throws {
-        XCTAssertEqual(try Day12(input: Day12.sample).solvePart2(), 29)
-    }
+        func test_part2_test() throws {
+            XCTAssertEqual(try Day12(input: Day12.sample).solvePart2(), 29)
+        }
 
-    func test_part2_solution() throws {
-        XCTAssertEqual(try Day12().solvePart2(), 332)
+        func test_part2_solution() throws {
+            XCTAssertEqual(try Day12().solvePart2(), 332)
+        }
     }
-}
 #endif
